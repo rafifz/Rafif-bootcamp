666bedbed314b414ccbebd448c53d5f4f31acf8a
rafifz <rafifz666@gmail.com>
2023-12-19 17:11:07 +0700

    Bootcamp day 2

--- day02/Animal.cs
+++ day02/Animal.cs
@@ -4,6 +4,12 @@ public class Animals
 	public int age;
 	public string name;
 	
+	public Animals(string name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+	
 	public void Eat(string food)
 	{
 		System.Console.WriteLine($"eating {food}");
--- day02/Horse.cs
+++ day02/Horse.cs
@@ -3,7 +3,12 @@ using System.Runtime.CompilerServices;
 using Animal;
 class Horse : Animals
 {
-	
-	
-	
+	public Horse (string name, int age)
+	{
+		 
+	}
+	public void kick(string food)
+	{
+		System.Console.WriteLine("horse is kicking");
+	}
 }
