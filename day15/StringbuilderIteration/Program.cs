d7b5c87229cfb8d0a46c326252c88264e67c0541
rafifz <rafifz666@gmail.com>
2024-01-15 13:25:09 +0700

    Garbage Collector

---
+++ day15/StringbuilderIteration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+        int iteration = 10000;
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for (int i = 0; i < iteration; i++)
+        {
+            sb.Append("Hello");
+            sb.Append("World");
+        }
+        stopwatch.Stop();
+
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+    }
+}
