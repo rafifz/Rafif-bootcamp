1de727b5bba5aeed2049aa0a800353801efcd583
rafifz <rafifz666@gmail.com>
2024-01-17 17:04:02 +0700

    day17

---
+++ day17/ConditionalCompilation/Program.cs
@@ -0,0 +1,10 @@
+﻿//#define DEBUG the default define from .net 
+#define BETA //will still return debug version, must be defined in console using `dotnet run -c release`
+
+#if DEBUG
+Console.WriteLine("Debug version is compiled");
+#elif BETA
+Console.WriteLine("Beta version is compiled");
+#elif RELEASE
+Console.WriteLine("Released version is compiled");
+#endif 
