7deb90e542e07245378fad80a2715d162978ca03
rafifz <rafifz666@gmail.com>
2024-01-17 23:01:39 +0700

    day 17

--- day17/ConditionalCompilation/Program.cs
+++ day17/ConditionalCompilation/Program.cs
@@ -1,10 +1,18 @@
-﻿//#define DEBUG the default define from .net 
-#define BETA //will still return debug version, must be defined in console using `dotnet run -c release`
+﻿//#define DEBUG -> this is by default define from .net
+#define RELEASE //will still return debug version, must be defined in console using `dotnet run -c release`
 
-#if DEBUG
-Console.WriteLine("Debug version is compiled");
-#elif BETA
-Console.WriteLine("Beta version is compiled");
-#elif RELEASE
-Console.WriteLine("Released version is compiled");
-#endif 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #if DEBUG
+        Console.WriteLine("Debug version is compiled");
+
+        #elif BETA
+        Console.WriteLine("Beta version is compiled");
+
+        #elif RELEASE
+        Console.WriteLine("Released version is compiled");
+        #endif
+    }
+}
---
+++ day17/DebugLogicError/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 5;
+        Multiply(a, b);
+    }
+
+    private static int Multiply(int a, int b)
+    {
+        return a + b; //logical error, should've been *
+        // to find this error in our program we need manually debug using debugger
+    }
+}
