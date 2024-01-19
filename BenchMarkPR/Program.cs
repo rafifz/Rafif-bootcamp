52052f3cb2051b0dcc7fdb17ad060d356aa94424
rafifz <rafifz666@gmail.com>
2024-01-19 14:31:30 +0700

    day18

---
+++ BenchMarkPR/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static async void Main(string[] args)
+    {
+        await MyMethod();
+    }
+
+    // public static Task MyMethod()
+    // {
+    //     return Task.Delay(2000);
+    // }
+
+    public static async Task MyMethod()
+    {
+        await Task.Delay(2000);
+    }
+}
--- Day18-master/Q_CancellationToken/Program.cs
+++ Day18-master/Q_CancellationToken/Program.cs
@@ -1,31 +1,37 @@
-﻿CancellationTokenSource cts = new();
-CancellationToken token = cts.Token;
-
-Task task = DoWorkAsync(token);
-
-Console.WriteLine("Press 'c' to cancel the operation.");
-if (Console.ReadKey().KeyChar == 'c')
+﻿internal class Program
 {
-	cts.Cancel();
-}
-
-try
-{
-	await task;
-	Console.WriteLine("Operation completed.");
-}
-catch (OperationCanceledException)
-{
-	Console.WriteLine("Operation canceled.");
-}
-
-static async Task DoWorkAsync(CancellationToken token)
-{
-    for (int i = 0; i < 10; i++)
+    private static async Task Main(string[] args)
     {
-        token.ThrowIfCancellationRequested();
+        CancellationTokenSource cts = new();
+        CancellationToken token = cts.Token;
 
-        Console.WriteLine($"Work in progress: {i * 10}%");
-        await Task.Delay(20000,token);
+        Console.WriteLine("Press 'c' to cancel the operation.");
+        Task task = DoWorkAsync(token);
+
+        if (Console.ReadKey().KeyChar == 'c')
+        {
+            cts.Cancel();
+        }
+
+        try
+        {
+            await task;
+            Console.WriteLine("Operation completed.");
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Operation canceled.");
+        }
+
+        static async Task DoWorkAsync(CancellationToken token)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                token.ThrowIfCancellationRequested();
+
+                Console.WriteLine($"Work in progress: {i * 10}%");
+                await Task.Delay(2000);
+            }
+        }
     }
 }
