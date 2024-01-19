54c3a39e1a41a7d53f3d3042af97e2ecff4af793
rafifz <rafifz666@gmail.com>
2024-01-19 14:33:18 +0700

    day19

---
+++ Day-19-master/21RaceConditionSolve/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+static class Program
+{
+	static int counter = 0;
+	static object asd = new object(); //Variable for locking
+	static async Task Main(string[] args)
+	{
+		var task1 = Task.Run(() => IncrementCounterAsync());
+		var task2 = Task.Run(() => IncrementCounterAsync());
+
+		await Task.WhenAll(task1, task2);
+
+		Console.WriteLine($"Counter: {counter}");
+		Console.ReadKey();
+	}
+
+	static async Task IncrementCounterAsync()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			lock (asd)
+			{
+				counter++;
+				Console.WriteLine($"Counter from: {counter}");
+			}
+			await Task.Delay(100);
+		}
+	}
+}
---
+++ Day-19-master/33_Semaphore/Program.cs
@@ -0,0 +1,43 @@
+﻿using Internal.Runtime.Augments;
+using System.Security.AccessControl;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreDemo
+{
+	class Program
+	{
+		
+		public static SemaphoreSlim semaphore = new SemaphoreSlim(3);
+
+		static async Task Main()
+		{
+			Task[] tasks = new Task[10];
+			for (int i = 1; i <= 10; i++)
+			{
+				int taskId = i;
+				tasks[i - 1] = Task.Run( () =>  DoSomeTask(taskId, i));
+			}
+
+			await Task.WhenAll(tasks);
+			Console.WriteLine("All task finished");
+		}
+
+		static async void DoSomeTask(int taskId, int index)
+		{
+			Console.WriteLine($"Task {taskId} Wants to Enter");
+			try
+			{   
+				semaphore.Wait();
+				Console.WriteLine("Success: Task " + taskId + " is Doing its work");
+				await Task.Delay(7000);
+				Console.WriteLine($"Task {taskId} Exit.");
+			}
+			finally
+			{
+				semaphore.Release();
+			}
+		}
+	}
+}
